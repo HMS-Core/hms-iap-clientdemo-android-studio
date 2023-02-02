d449ee09d254b1422a284433415730ae85bd7468
fjl2022 <657552147@qq.com>
2023-02-02 18:26:35 +0800

    Update SubscriptionActivity.java

--- app/src/main/java/com/huawei/iapdemo/subscription/SubscriptionActivity.java
+++ app/src/main/java/com/huawei/iapdemo/subscription/SubscriptionActivity.java
@@ -19,12 +19,14 @@ package com.huawei.iapdemo.subscription;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.huawei.hms.iap.entity.OrderStatusCode;
 import com.huawei.hms.iap.entity.OwnedPurchasesResult;
 import com.huawei.hms.iap.entity.ProductInfo;
