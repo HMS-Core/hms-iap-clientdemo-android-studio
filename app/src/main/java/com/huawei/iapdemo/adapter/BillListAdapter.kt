d2182c15407ca65df0671e667707d261d1fdcd16
fjl2022 <657552147@qq.com>
2023-05-25 10:24:59 +0800

    Update BillListAdapter.kt

--- app/src/main/java/com/huawei/iapdemo/adapter/BillListAdapter.kt
+++ app/src/main/java/com/huawei/iapdemo/adapter/BillListAdapter.kt
@@ -24,7 +24,7 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
 import com.huawei.hms.iap.entity.InAppPurchaseData
-import com.iapdemo.huawei.R
+import com.huawei.iapdemo.R
 import org.json.JSONException
 import org.json.JSONObject
 
